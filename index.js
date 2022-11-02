9cec4d6b606d6141e19177ae69a1cdda02c63433
frank <franksavena@gmail.com>
2022-11-01 20:24:28 -0500

    commit tree 9fdd741b027472a345e44afdc219730fee081a90

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const gifts = ["Teddy bear", "drone", "Doll"];
+const messages = [];
+
+function writeCards(names, event) {
+    console.log(names, event);
+    for (let i = 0; i < names.length; i++) {
+         messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0){
+        console.log(number)
+        number--
+    }
+}
